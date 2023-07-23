e4d6a99df7a2ce694241ff2f364ee3c4ed06eb5f
cccccmake <kechou.dai@gmail.com>
2023-07-23 16:59:09 +0200

    update part tree code

--- Tree/530/Solution.java
+++ Tree/530/Solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public int getMinimumDifference(TreeNode root) {
-        int res = 0;
-        if(root == null) return res;
-        int leftRes = Math.abs(root.val - findNearest(root.left, 0));
-        int rightRes = Math.abs(root.val - findNearest(root.right, 1));
-        return leftRes <= rightRes ? leftRes : rightRes;
+        List<Integer> res = new ArrayList<>();
+        traverse(root, res);
+        int minimumDiff = Integer.MAX_VALUE;
+        for(int i = 0; i < res.size(); i++){
+            int j = i + 1;
+            // need to consider the boundary overflow
+            if(j < res.size() && Math.abs(res.get(j) - res.get(i)) < minimumDiff)
+                // update the minimumDiff
+                minimumDiff = Math.abs(res.get(j) - res.get(i));
+        }
+        // if there is only 0/1 node in a tree, the res should be 0
+        return minimumDiff == Integer.MAX_VALUE ? 0 : minimumDiff;
     }
 
-    public int findNearest(TreeNode root, int direction){
-        if(root == null) return Integer.MAX_VALUE;
-        if(direction == 0){
-            while(root != null)
-                root = root.right;
-        }
-        else{
-            while(root != null)
-                root = root.left;
-        }
-        return root.val;
+    public void traverse(TreeNode root, List<Integer> res){
+        // traverse the tree and save the values into an array list
+        if(root == null) return;
+        traverse(root.left, res);
+        res.add(root.val);
+        traverse(root.right, res);
     }
 }
--- Tree/98/Solution.java
+++ Tree/98/Solution.java
@@ -1,17 +1,65 @@
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         if(root == null) return true;
+//         if(root.val > root.left.val && root.val < root.right.val) return recursion(root.left, root.val, 0) && recursion(root.right, root.val, 1);
+//         return false;
+//     }
+
+//     public boolean recursion(TreeNode root, int reference, int direction){ // direction = 0 -> left =1 -> right
+//         if(root == null) return true;
+//         if(direction == 0){
+//             return root.val < reference && recursion(root.left, root.val, 0) && recursion(root.right, reference, 1);
+//         }
+//         else{
+//             return root.val > reference && recursion(root.left, root.val, 0) && recursion(root.right, reference, 1);
+//         }
+//     }
+// }
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         int maxVal = Integer.MIN_VALUE;
+//         return traverse(root, maxVal);
+//     }
+//     public boolean traverse(TreeNode root, int maxVal){
+//         if(root == null) return true;
+//         boolean left = traverse(root.left, maxVal);
+//         if(maxVal < root.val) maxVal = root.val;
+//         else return false;
+//         boolean right = traverse(root.right, maxVal);
+//         return left && right;
+//     }
+// }
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         int maxVal = Integer.MIN_VALUE;
+//         TreeNode max = new TreeNode(Integer.MIN_VALUE);
+//         return traverse(root, max);
+//     }
+//     public boolean traverse(TreeNode root, TreeNode max){
+//         if(root == null) return true;
+//         boolean left = traverse(root.left, max);
+//         if(max.val < root.val) max.val = root.val;
+//         else return false;
+//         boolean right = traverse(root.right, max);
+//         return left && right;
+//     }
+// }
+
+// Using mid-order traverse to check whether the traverse is ordered
 class Solution {
+    TreeNode max;
+    // the obj is default null
+    // [1] TreeNode max = new TreeNode(Integer.MIN_VALUE);
     public boolean isValidBST(TreeNode root) {
         if(root == null) return true;
-        if(root.left != null && root.right == null && )
-        if(root.val > root.left.val && root.val < root.right.val) return recursion(root, root.val, 0) && recursion(root, root.val, 1);
-    }
-
-    public boolean recursion(TreeNode root, int reference, int direction){ // direction = 0 -> left =1 -> right
-        if(root == null) return true;
-        if(direction == 0){
-            return root.val < reference && recursion(root.left, root.val, 0) && recursion(root.right, reference, 1);
-        }
-        else{
-            return root.val > reference && recursion(root.left, root.val, 0) && recursion(root.right, reference, 1);
-        }
+        boolean left = isValidBST(root.left);
+        // [1] the test case [Integer.MIN_VALUE] won't pass
+        // if(max.val >= root.val)
+        if(max != null && max.val >= root.val) return false;
+        else max = root;
+        boolean right = isValidBST(root.right);
+        return left && right;
     }
 }
