7ba806bc8fd9aaa79004c91432b280b4d15a337e
cccccmake <kechou.dai@gmail.com>
2023-09-27 23:20:27 +0200

    update

--- Dyncmic Programming/62/Solution.java
+++ Dyncmic Programming/62/Solution.java
@@ -3,14 +3,14 @@ class Solution {
         int[][] dpArray = new int[m][n];
         dpArray[0][0] = 1;
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < m; j++){
+            for(int j = 0; j < n; j++){
                 if(i > 0 && j > 0)
                     dpArray[i][j] = dpArray[i-1][j] + dpArray[i][j-1];
                     // must i comes first, then j
-                else if(i == 0)
-                    dpArray[i][j] = dpArray[i][j-1];
-                else if(j == 0)
+                else if(i > 0)
                     dpArray[i][j] = dpArray[i - 1][j];
+                else if(j > 0)
+                    dpArray[i][j] = dpArray[i][j - 1];
             }
         }
         return dpArray[m - 1][n - 1];
