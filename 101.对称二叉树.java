2ef6be64c872cacec497ce98dc4a8e69b6863ecc
cccccmake <kechou.dai@gmail.com>
2024-01-11 18:40:15 +0100

    more comments for 101

--- 101.对称二叉树.java
+++ 101.对称二叉树.java
@@ -22,13 +22,23 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        // an empty tree is already symmetric. No one knows why :)
         if(root == null)
             return true;
         return recur(root.left, root.right);
     }
 
+    /**
+     * recursion function, return whether the trees with roots L and R
+     * are symmetric
+     * @param L
+     * @param R
+     * @return true or false
+     */
     public boolean recur(TreeNode L, TreeNode R) {
+        // if the parent of nodes L and R is already a leaf
         if (L == null && R == null) return true;
+        // otherwise, the tree contains L, R, root is not symmetric
         if (L == null || R == null || L.val != R.val) return false;
         return recur(L.left, R.right) && recur(L.right, R.left);
     }
