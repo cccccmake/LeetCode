223c92d86ec8b37cc7c20b2eaadd753c4c9fd412
cccccmake <kechou.dai@gmail.com>
2024-01-11 18:42:31 +0100

    more comments for 215 => principle declaration

--- 215.数组中的第k个最大元素.java
+++ 215.数组中的第k个最大元素.java
@@ -11,11 +11,16 @@ import java.util.PriorityQueue;
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // using a minimal heap to store the top-k elements
         for(int i = 0; i < k; i++){
             minHeap.offer(nums[i]);
         }
+
         for(int i = k; i < nums.length; i++){
+            // the top elemtn of the heap is the smallest one among k elements
             int heapElem = minHeap.peek();
+            // if current element < nums[i], remove the current element and insert
+            // nums[i] into the heap
             if(heapElem < nums[i]){
                 minHeap.poll();
                 minHeap.offer(nums[i]);
